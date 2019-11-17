5d4b9649e40f226477908da2986811058c7dc2da
BrunoCecco <bruno.ceccolini@gmail.com>
2019-11-17 10:10:38 +0000

    update

--- Python tasks/PyGame/invaders.py
+++ Python tasks/PyGame/invaders.py
@@ -12,8 +12,14 @@ size = (640, 480)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Snow")
 done = False
-
-
+font = pygame.font.Font(None, 30)
+            
+def print_text(x_pos, y_pos, my_screen, text_string):
+    #Draw text onto the screen
+    text_map = font.render(str(text_string), True, WHITE)
+    my_screen.blit(text_map, [x_pos, y_pos])
+    #y_pos += line_height
+ 
 class Player(pygame.sprite.Sprite):
     # Define the constructor for invaders
     def __init__(self, color, width, height):
@@ -27,12 +33,15 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 300
         self.rect.y = size[1]-height
+        self.rect.topleft = self.rect.x, self.rect.y
+        self.width = width
+        self.height = height
         self.speed = 0
-        
-     #End Procedure
-        
-    score = 0
-    def score_update(self, val):
+        self.score = 0 # ordinary attribute
+        self.bullet_count = 50
+        self.lives = 5
+
+    def increase_score(self, val):
         self.score += val
         
     def player_set_speed(self, val):
@@ -45,8 +54,9 @@ class Player(pygame.sprite.Sprite):
         elif self.rect.x <= 0:
             self.rect.x = 0
 
-    def num_bullets(self, bullet_count):
-        bullet_count -= 1
+    def decrease_bullets(self):
+        if self.bullet_count> 0:
+            self.bullet_count -= 1
 
 
 ## -- Define the class invaders which is a sprite
@@ -63,8 +73,9 @@ class Invader(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(0, 600)
         self.rect.y = random.randrange(-100, 0)
-     #End Procedure
-
+        self.rect.topleft = self.rect.x, self.rect.y
+        self.width = width
+        self.height = height
         self.speed = speed
         
     def update(self):
@@ -81,58 +92,71 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player.rect.x
         self.rect.y = player.rect.y
-        self.speed = 2
+        self.rect.topleft = self.rect.x, self.rect.y
+        self.width = width
+        self.height = height
+        self.speed = 1
 
     def update(self):
         self.rect.y = self.rect.y - self.speed
+        if self.rect.y < 0:
+            bullet_group.remove(self)
 
-
-bullet_count = 50
 
 all_sprites_group = pygame.sprite.Group()
 invader_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
 
-player = Player(YELLOW, 5, 5)
+player = Player(YELLOW, 10, 10)
 all_sprites_group.add(player)
 
-# Create the snowflakes
-number_of_invaders = 10 # we are creating 50 snowflakes
+# Create the invaders
+number_of_invaders = 10
 for x in range (number_of_invaders):
-    invaders = Invader(BLUE, 5, 5, 1) # snowflakes are white with size 10 by 10
-    invader_group.add(invaders) # adds the new snowflake to the group of snowflakes
-    all_sprites_group.add(invaders) # adds it to the group of all Sprites
+    invader = Invader(BLUE, 20, 20, 1) 
+    invader_group.add(invader)
+    all_sprites_group.add(invader)
 #Next x
 
 clock = pygame.time.Clock()
+last_keydown = 0
 
-while not done:
+while not done and player.lives > 0:
 #-user input and controls            
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    if event.type == pygame.KEYDOWN: # - a key is down
-        if event.key == pygame.K_LEFT: # - if the left key pressed
-            player.player_set_speed(-3) # speed set to -3
-        if event.key == pygame.K_RIGHT: # - if the right key pressed
-            player.player_set_speed(3) # speed set to 3
-        if event.key == pygame.K_UP:
-            bullet = Bullet(RED, 2, 2)
-            all_sprites_group.add(bullet)
-            bullet_group.add(bullet)
-    elif event.type == pygame.KEYUP:
-        player.player_set_speed(0) # speed set to 0
-        
-    for bullets in bullet_group:
-        player.num_bullets(bullet_count)            
-        bullet_hit_group = pygame.sprite.spritecollide(bullet, invader_group, True)
-        for bullet in bullet_hit_group:
-            player.score_update(5)
-
-                
+        if event.type == pygame.KEYDOWN: # - a key is down
+            curr_time = pygame.time.get_ticks()
+            if event.key == pygame.K_LEFT: # - if the left key pressed                
+                player.player_set_speed(-3) # speed set to -3
+            if event.key == pygame.K_RIGHT: # - if the right key pressed
+                player.player_set_speed(3) # speed set to 3
 
+            if curr_time - last_keydown > 100: # limit frequency of shooting
+                if event.key == pygame.K_UP:
+                    if player.bullet_count > 0:
+                        bullet = Bullet(RED, 5, 5)
+                        all_sprites_group.add(bullet)
+                        bullet_group.add(bullet)
+                        player.decrease_bullets()
+                        last_keydown = curr_time
+                        
+        elif event.type == pygame.KEYUP:
+            player.player_set_speed(0) # speed set to 0
+
+    player_hit_group = pygame.sprite.spritecollide(player, invader_group, True)
+    for p in player_hit_group:
+        player.lives -= 1
         
+    for b in bullet_group:         
+        invader_hit_group = pygame.sprite.spritecollide(b, invader_group, True)
+        for b in invader_hit_group:
+            player.increase_score(5)
+
+    if player.lives == 4:
+        print_text(250, 150, screen, "Game Over")
     # -- Game logic goes in here
     all_sprites_group.update()
     # -- Screen background is BLACK
@@ -140,10 +164,14 @@ while not done:
     # -- Drawing code goes here
     all_sprites_group.draw(screen)
 
-# -- flip display to reveal new position of objects
+    print_text(20, 20, screen, "Lives: %d" % player.lives)
+    print_text(20, 50, screen, "Score: %d" % player.score)
+    print_text(20, 80, screen, "Bullets: %d" % player.bullet_count)
+    # -- flip display to reveal new position of objects
     pygame.display.flip()
- # - The clock ticks over
+    # - The clock ticks over
     clock.tick(60)
+
+
 #End While - End of game loop
 pygame.quit()
-
