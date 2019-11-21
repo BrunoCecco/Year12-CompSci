8f88a6d837da05370bdbeae507e91fb80e7d507b
BrunoCecco <bruno.ceccolini@gmail.com>
2019-11-21 12:27:49 +0000

    update

---
+++ Python tasks/PyGame/snow.py
@@ -0,0 +1,76 @@
+import pygame
+import math
+import random
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (50, 50, 255)
+YELLOW = (255, 255, 0)
+ORANGE = (245, 154, 17)
+pygame.init()
+size = (640, 480)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Snow")
+done = False
+
+
+## -- Define the class snow which is a sprite
+class snow(pygame.sprite.Sprite):
+    # Define the constructor for snow
+    def __init__(self, color, width, height, speed):
+        
+        # Call the sprite constructor
+        super().__init__()
+        # Create a sprite and fill it with colour
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        # Set the position of the sprite
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, 600)
+        self.rect.y = random.randrange(0, 400)
+     #End Procedure
+
+        self.speed = speed
+        
+    def update(self):
+        self.rect.y = self.rect.y + self.speed
+
+
+#End Class
+
+# Create a list of all snow blocks
+snow_group = pygame.sprite.Group()
+# Create a list of all sprites
+all_sprites_group = pygame.sprite.Group()
+
+# Create the snowflakes
+number_of_flakes = 50 # we are creating 50 snowflakes
+for x in range (number_of_flakes):
+    my_snow = snow(WHITE, 5, 5, 1) # snowflakes are white with size 5 by 5 px
+    snow_group.add (my_snow) # adds the new snowflake to the group of snowflakes
+    all_sprites_group.add (my_snow) # adds it to the group of all Sprites
+#Next x
+
+
+clock = pygame.time.Clock()
+
+while not done:
+# -- User input and controls
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        # -- Game logic goes in here
+    all_sprites_group.update()
+    # -- Screen background is BLACK
+    screen.fill(BLACK)
+    # -- Drawing code goes here
+    all_sprites_group.draw(screen)
+
+# -- flip display to reveal new position of objects
+    pygame.display.flip()
+ # - The clock ticks over
+    clock.tick(60)
+#End While - End of game loop
+pygame.quit()
+
